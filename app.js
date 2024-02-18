32e4eea336f4b10c9a17c1cc4ed8411ed6889f86
22P35A0384 <sai.gangadhar2022@outlook.com>
2024-02-18 17:12:49 +0530

    5:12 pm

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import http from "http"; // Import the 'http' module
 import mongoose from "mongoose";
 import bodyParser from 'body-parser';
 import cors from 'cors';
@@ -18,11 +19,20 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors())
 mongoose.connect("mongodb+srv://gangadharjami:Xyd11TsTf0GfkNLu@cluster0.egjdqci.mongodb.net/cluster0?retryWrites=true&w=majority")
-    .then(()=>app.listen(5000))
-    .then(()=>
-        console.log('Database Connected && Server Started Successfully!! :)')
-    )
-    .catch((err)=>console.log(err));
+    .then(() => {
+        // Create HTTP server using 'http.createServer'
+        const server = http.createServer(app);
+        
+        // Specify the port and hostname for the server to listen on
+        const port = process.env.PORT || 5000;
+        const hostname = '0.0.0.0';
+
+        server.listen(port, hostname, () => {
+            console.log(`Server running at http://${hostname}:${port}/`);
+            console.log('Database Connected && Server Started Successfully!! :)');
+        });
+    })
+    .catch((err) => console.log(err));
 
 // Frontend Is Yours, But Backend Was Not, It Was Mine If You Touch It Then It Will Hurt You Very Hard, A Sweet Warning For Next Developer.... :) @Gangadhar Jami
 
