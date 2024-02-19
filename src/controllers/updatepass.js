81eea5a8ee045e255d157fd4daf4fbd2f4994bb6
22P35A0384 <sai.gangadhar2022@outlook.com>
2024-02-19 14:11:55 +0530

    19/02/2024 02:11 pm

--- src/controllers/updatepass.js
+++ src/controllers/updatepass.js
@@ -1,20 +1,23 @@
 import Userschema from '../models/newuser.js';
+import bcrypt from 'bcryptjs';
 
 const Updatepass = async (req, res, next) => {
     const mail = req.body.email;
     const newPassword = req.body.newpass; // Corrected destructuring
+
+    const hashedPassowrd = bcrypt.hashSync(newPassword)
     
-    console.log(mail);
-    console.log(req.body);
+    // console.log(mail);
+    // console.log(req.body);
     
     try {
         const data = await Userschema.findOne({ email: mail });
         const _id = data._id;
         
-        const findAndUpdate = await Userschema.findByIdAndUpdate(_id, { password: newPassword }); // Using newPassword directly
+        const findAndUpdate = await Userschema.findByIdAndUpdate(_id, { password: hashedPassowrd }); // Using newPassword directly
         
-        console.log(data);
-        console.log(findAndUpdate);
+        // console.log(data);
+        // console.log(findAndUpdate);
         
         if (findAndUpdate) {
             return res.status(200).json(true);
