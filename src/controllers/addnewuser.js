dd18b13bcfb533038e15f146f273f291c959ccbf
22P35A0384 <sai.gangadhar2022@outlook.com>
2024-02-25 10:57:48 +0530

    25/02/2024 10:57 am

--- src/controllers/addnewuser.js
+++ src/controllers/addnewuser.js
@@ -4,52 +4,52 @@ import bcrypt from 'bcryptjs';
 import fs from 'fs';
 import handlebars from 'handlebars';
 
+const Addnewuser = async (req, res, next) => {
+    const profile = (req.file) ? req.file.filename : null;
+    const { fname, lname, password, email } = req.body;
 
-const Addnewuser = async(req,res,next)=>{
-    // console.log(req.body)
-    const profile = (req.file) ? req.file.filename : null
-    const {fname,lname, password, email} = req.body;
-    const hashedPassowrd = bcrypt.hashSync(password)
-    const formdata = new Newuserschema({
-        fname,
-        lname,
-        password:hashedPassowrd,
-        email,
-        profile 
-    })
-    try{
-        formdata.save()
-        var transporter = nodemailer.createTransport({
+    try {
+        // Hash the password
+        const hashedPassword = bcrypt.hashSync(password, 10); // You can specify the number of salt rounds (e.g., 10)
+
+        // Create a new user document
+        const formdata = new Newuserschema({
+            fname,
+            lname,
+            password: hashedPassword,
+            email,
+            profile
+        });
+
+        // Save the new user data to the database
+        await formdata.save();
+
+        // Send a welcome email
+        const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-              user: 'technicalhubdriverready@gmail.com',
-              pass: 'aqlp joww mqgk fmbw'
+                user: 'technicalhubdriverready@gmail.com',
+                pass: 'aqlp joww mqgk fmbw'
             }
         });
         const source = fs.readFileSync('public/mail_templates/welcomemail.hbs', 'utf8');
         const template = handlebars.compile(source);
-        const Name = fname+" "+lname
-        // Render the template with OTP
-        const html = template({Name});
-        var mailOptions = {
+        const Name = `${fname} ${lname}`;
+        const html = template({ Name });
+        const mailOptions = {
             from: 'technicalhubdriverready@gmail.com',
             to: email,
             subject: 'Welcome To ASTE',
-            html:html
+            html: html
         };
-          
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-        return res.send({msg:'Your Account Was Created, You Will Be Redirected To Login Page'})      
-    }catch(err){
-        console.log(err)
+        await transporter.sendMail(mailOptions);
+        
+        // Respond with a success message
+        return res.send({ msg: 'Your Account Was Created, You Will Be Redirected To Login Page' });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
-    return res.status(200).json({formdata})
-}
+};
 
 export default Addnewuser;
